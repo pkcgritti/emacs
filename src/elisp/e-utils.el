f1232f9aac8ea3cb9650951a09b551446fb07449
Marcos Cesar Gritti <cesargritti@gmail.com>
2019-06-13 00:08:09 -0300

    New configs for clojure-mode

--- src/elisp/e-utils.el
+++ src/elisp/e-utils.el
@@ -26,8 +26,7 @@
   "Open neotree using the project root, using projectile or the current buffer directory"
   (interactive)
   (let ((project-dir (ignore-errors (projectile-project-root)))
-	(file-name (buffer-file-name))
-	(neo-smart-open t))
+	(file-name (buffer-file-name)))
     (if (neo-global--window-exists-p)
 	(neotree-hide)
       (progn
