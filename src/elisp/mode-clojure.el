d168e8743fa5ecb5ca3f6b42c24cde0048ac9bfe
Marcos Cesar Gritti <cesargritti@gmail.com>
2019-06-19 17:41:31 -0300

    Novas atualizações

--- src/elisp/mode-clojure.el
+++ src/elisp/mode-clojure.el
@@ -13,10 +13,22 @@
 (evil-leader/set-key-for-mode 'clojure-mode "r a r" 'cljr-add-require-to-ns)
 (evil-leader/set-key-for-mode 'clojure-mode "r a i" 'cljr-add-import-to-ns)
 (evil-leader/set-key-for-mode 'clojure-mode "r a p" 'cljr-add-project-dependency)
+(define-key clojure-mode-map (kbd "M-/") 'hippie-expand)
 
 (evil-leader/set-key-for-mode 'cider-mode "s r" 'cider-refresh)
 (evil-leader/set-key-for-mode 'cider-mode "s q" 'cider-quit)
 
+;; TODO: Check if filepath does not change on windows.
+;; (setq e:lein-profile-path "~/.lein/profiles.clj")
+
+;; (defun e:configure-repl-profile ()
+;;   (interactive)
+;;   (if (not (file-exists-p e:lein-profile-path))
+;;       (with-temp-file e:lein-profile-path
+;; 	(insert ":plugins [[refactor-nrepl       \"2.5.0-SNAPSHOT\"]
+;;           [cider/cider-nrepl    \"0.22.0-beta5\"]]\n"))))
+;; (e:configure-repl-profile)
+	
 (defun e:configure-cider-plugins ()
   "Configure cider variables"
   (setq cider-jack-in-lein-plugins
